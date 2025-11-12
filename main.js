2927cde25d426bc1a34107befe808706027c5e69
Arthur Vincent Simon <developarvin@gmail.com>
2025-11-12 20:32:17 +0800

    Generate background colors

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
+// Generate a random color that is not too light
+function generateColors() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setBackgroundColor() {
+    const color = generateColors();
+    document.getElementById('background').style.backgroundColor = color;
+}
+
+
 function displayRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[randomIndex];
     document.getElementById('quote').innerText = `${quote.quote}`;
     document.getElementById('author').innerText = `- ${quote.author}`;
+}
+
+function runOnLoad() {
+    setBackgroundColor();
+    displayRandomQuote();
+
 }
